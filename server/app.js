d09f295bfe302feb58524bb507f9691c1d8c531d
Jona Hyun <jona.hyun@gmail.com>
2017-01-30 14:56:01 -0500

    testing api and corrected package.json on both client and server for local mac because the path or forwards vs backward slash

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 var express=require('express');
 var app=express();
 
+// create test api
+app.use('/api/test', function(req, res){
+	res.send("Hello World");
+});
+
 app.listen(3003,function(){
 	console.log("app is listening to 3003");
 });
