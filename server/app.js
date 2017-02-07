42651b7248e958bacd19a4785873a9c0c8cece06
Jona Hyun <jona.hyun@gmail.com>
2017-02-07 06:26:08 -0500

    added serlog

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ app.use(require('./middleware/validate-session'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/login', require('./routes/session'));
 app.use('/api/definition', require('./routes/definition'));
+app.use('/api/log', require('./routes/log'));
 
 
 
